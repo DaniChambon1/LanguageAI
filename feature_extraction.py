6023b8001d74ef4110ae85fcb7da897576d0f140
isavgt <isavogten@gmail.com>
2023-12-13 14:47:16 +0100

    Added dropping features

--- feature_extraction.py
+++ feature_extraction.py
@@ -11,17 +11,17 @@ class FeatureExtractor:
         self.new_path = new_path
 
         # Read in the original data
-        self.original_df = pd.read_csv(original_path)
+        self.original_df = pd.read_csv(original_path, index_col=[0])
 
     def add_feature(self, df_feature):
         # Read data + already existing features
-        df_new = pd.read_csv(self.new_path)
+        df_new = pd.read_csv(self.new_path, index_col=[0])
 
         # Join new feature on existing features
-        df_new = df_new.join(df_feature, lsuffix = 'index', rsuffix='index')
+        df_new = df_new.join(df_feature)
 
         # Write new df with the feature to csv
-        df_new.to_csv(self.new_path)
+        df_new.to_csv(self.new_path, index = True)
     
     def word_count(self):
         # Copy original df
@@ -31,13 +31,22 @@ class FeatureExtractor:
         df_feature["word_count"] = df_feature["post"].apply(lambda n: len(n.split()))
 
         # Select only index and feature
-        df_feature = df_feature[["index","word_count"]]
+        df_feature = df_feature[["word_count"]]
 
         # Add feature to csv
         self.add_feature(df_feature)
+    
+    def drop_features(self, column_names: list): 
+        df_features = pd.read_csv(self.new_path, index_col=[0])
+        print(df_features)
+        df_features = df_features.drop(columns= column_names)
+        print(df_features)
+        df_features.to_csv(self.new_path)
+
 
 # Create an instance of the Feature Extractor with specified paths
 FE = FeatureExtractor("data/cleaned_combined_data_english.csv", "data/final_combined_data_english.csv")
 
 # Execute the function word_count and add the feature to the final dataset.
-FE.word_count()
+# FE.word_count()
+FE.drop_features(['word_count'])
