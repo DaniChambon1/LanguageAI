ec88d3d95972db9c61b2d4eeba6172e5a3951f19
20203171 <h.l.storms@student.tue.nl>
2023-12-15 10:03:35 +0100

    added kNN

--- feature_hilde.py
+++ feature_hilde.py
@@ -3,31 +3,40 @@ import re
 from data_cleaning import combined_gen
 
 def capital(dataset):
-    count_list = []
-    stand_list = []
+    count_capitals = []
+    stand_capitals = []
     for i in range(len(dataset)):
         count = 0
         for char in dataset['post'][i]:
             if char.isupper():
                 count += 1
-        count_list.append(count)
-        stand_list.append(count / len(dataset['post'][i]))
-    return count_list, stand_list
+        count_capitals.append(count)
+        stand_capitals.append(count / len(dataset['post'][i]))
+    return count_capitals, stand_capitals
 
 
 def emoticons(dataset):
     count_emoticons = []
+    stand_emoticons = []
     for i in range(len(dataset)) :  
-        emoticon_pattern = r'(:-?\))'
+        emoticon_pattern = r'(:-?\)|:-?D|;-?\)|:-?P|:-?\(|:-?\/|:-?O|<3)'
         emoticons = re.findall(emoticon_pattern, dataset['post'][i])
-        if emoticons:
-            count_emoticons.append(1)
-        else:
-            count_emoticons.append(0)
-            
-    return count_emoticons
+        count_emoticons.append(len(emoticons))
+        stand_emoticons.append(len(emoticons) / len(dataset['post'][i]))
+    return count_emoticons, stand_emoticons
+
+def pronouns(dataset):
+    count_pronouns = []
+    stand_pronouns = []
+    for i in range(len(dataset)) :  
+        pronoun_pattern = r'\b(?:he|she|it|they|we|you|I|me|my|mine|you|your|yours|him|her|hers|us|our|ours|them|their|theirs)\b'
+        pronouns = re.findall(pronoun_pattern, dataset['post'][i])
+        count_pronouns.append(len(pronouns))
+        stand_pronouns.append(len(pronouns) / len(dataset["post"][i].split()))
+    return count_pronouns, stand_pronouns
 
 
 
 combined_gen['capital count'], combined_gen['capital count (stand.)'] =  capital(combined_gen)
-combined_gen['emoticon presence'] = emoticons(combined_gen)
+combined_gen['emoticon count'], combined_gen['emoticon count (stand.)'] = emoticons(combined_gen)
+combined_gen['pronoun count'], combined_gen['pronoun count (stand.)'] = pronouns(combined_gen)
--- kNN.py
+++ kNN.py
@@ -1,5 +1,5 @@
 from feature_hilde import combined_gen
-from sklearn import svm
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score
 
@@ -7,11 +7,11 @@ X = combined_gen[["capital count","capital count (stand.)","emoticon count","emo
 y = combined_gen["Millennial"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-model = svm.SVC(kernel='linear')
+model = KNeighborsClassifier(n_neighbors=2)
 model.fit(X_train, y_train)
 predictions = model.predict(X_test)
 accuracy = accuracy_score(y_test, predictions)
 precision = precision_score(y_test, predictions)
 recall = recall_score(y_test, predictions)
 f1 = f1_score(y_test, predictions)
-print(f"SVM --> Accuracy: {accuracy}, Precision: {precision}, Recall: {recall}, F1-score: {f1}")
+print(f"kNN --> Accuracy: {accuracy}, Precision: {precision}, Recall: {recall}, F1-score: {f1}")
