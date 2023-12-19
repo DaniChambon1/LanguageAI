f68c5912483d439a890eb091e7d211305a568d33
20203171 <h.l.storms@student.tue.nl>
2023-12-19 12:15:20 +0100

    add gender to majority

--- baseline.py
+++ baseline.py
@@ -1,7 +1,13 @@
 from feature_hilde import combined_gen
 from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score
 
-majority_predict = [combined_gen['Millennial'].mode()[0]]*len(combined_gen)
+majority_predict = []
+for i in range(len(combined_gen)):
+    if combined_gen['female'][i] == 1:
+        majority_predict.append([combined_gen['Millennial'][combined_gen['female'] == 1].mode()[0]])
+    else:
+        majority_predict.append([combined_gen['Millennial'][combined_gen['female'] == 0].mode()[0]])
+
 
 accuracy = accuracy_score(combined_gen['Millennial'], majority_predict)
 precision = precision_score(combined_gen['Millennial'], majority_predict)
