a41c6e1aa12db88473e0098d24f28074e40da19d
20203171 <h.l.storms@student.tue.nl>
2023-12-19 11:57:38 +0100

    mapje data

--- data_cleaning.py
+++ data_cleaning.py
@@ -2,8 +2,7 @@ import pandas as pd
 import numpy as np
 # birth_year = pd.read_csv("lai-data/birth_year.csv")
 # gender = pd.read_csv("lai-data/gender.csv")
-combined = pd.read_csv("combined_data_english.csv")
-
+combined = pd.read_csv("data\combined_data_english.csv")
 generations = []
 for i in range(len(combined)):
     if combined['birth_year'][i] in range(1980, 1997):
