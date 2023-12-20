cc6fb9432c56433cb514835dfe7bb3ace0043b3c
20203171 <h.l.storms@student.tue.nl>
2023-12-20 10:19:08 +0100

    combined all features

--- feature_giulia.py
+++ feature_giulia.py
@@ -1,1 +1,28 @@
-print('test')
+import pandas
+import re
+from data_cleaning import combined_gen as df
+
+def contraction(dataset):
+    contraction = '[a-zA-Z]+\'m\s|[a-zA-Z]+\'d\s|[a-zA-Z]+\'ll\s|[a-zA-Z]+\'re\s|[a-zA-Z]+\'ve\s|[a-zA-Z]+\'s\s|[a-zA-Z]+\'t\s'
+    lst_contractions = []
+    for i in range(len(dataset)):
+        post = dataset['post'][i]
+        find_contraction = re.findall(contraction, post)
+        total_contractions_sentence = len(find_contraction)
+        lst_contractions.append(total_contractions_sentence)
+    return lst_contractions
+
+
+def exaggerate(dataset):
+    exaggeration = '[a|A]{3,}|[b|B]{3,}|[c|C]{3,}|[d|D]{3,}|[e|E]{3,}|[f|F]{3,}|[g|G]{3,}|[h|H]{3,}|[i|I]{3,}|[j|J]{3,}|[k|K]{3,}|[l|L]{3,}|[m|M]{3,}[n|N]{3,}|[o|O]{3,}|[p|P]{3,}|[q|Q]{3,}|[r|R]{3,}|[s|S]{3,}|[t|T]{3,}|[u|U]{3,}|[v|V]{3,}|[w|W]{3,}|[x|X]{3,}|[y|Y]{3,}|[z|Z]{3,}|[.]{2,}|[!]{2,}'
+    lst_exaggeration = []
+    for i in range(len(dataset)):
+        post = dataset['post'][i]
+        find_exaggeration = re.findall(exaggeration, post)
+        total_exaggeration_sentence = len(find_exaggeration)
+        lst_exaggeration.append(total_exaggeration_sentence)
+    return lst_exaggeration
+
+
+df['contraction'] = contraction(df)
+df['exaggerate'] = exaggerate(df)
--- feature_hilde.py
+++ feature_hilde.py
@@ -1,42 +1,95 @@
 import pandas as pd
 import re
+from tqdm import tqdm
+from langdetect import detect
+import string
 from data_cleaning import combined_gen
 
 def capital(dataset):
-    count_capitals = []
     stand_capitals = []
     for i in range(len(dataset)):
         count = 0
         for char in dataset['post'][i]:
             if char.isupper():
                 count += 1
-        count_capitals.append(count)
         stand_capitals.append(count / len(dataset['post'][i]))
-    return count_capitals, stand_capitals
+    return stand_capitals
 
 
 def emoticons(dataset):
-    count_emoticons = []
     stand_emoticons = []
     for i in range(len(dataset)) :  
         emoticon_pattern = r'(:-?\)|:-?D|;-?\)|:-?P|:-?\(|:-?\/|:-?O|<3)'
         emoticons = re.findall(emoticon_pattern, dataset['post'][i])
-        count_emoticons.append(len(emoticons))
         stand_emoticons.append(len(emoticons) / len(dataset['post'][i]))
-    return count_emoticons, stand_emoticons
+    return stand_emoticons
 
 def pronouns(dataset):
-    count_pronouns = []
     stand_pronouns = []
     for i in range(len(dataset)) :  
         pronoun_pattern = r'\b(?:he|she|it|they|we|you|I|me|my|mine|you|your|yours|him|her|hers|us|our|ours|them|their|theirs)\b'
         pronouns = re.findall(pronoun_pattern, dataset['post'][i])
-        count_pronouns.append(len(pronouns))
         stand_pronouns.append(len(pronouns) / len(dataset["post"][i].split()))
-    return count_pronouns, stand_pronouns
+    return stand_pronouns
 
 
+def contraction(dataset):
+    contraction = '[a-zA-Z]+\'m\s|[a-zA-Z]+\'d\s|[a-zA-Z]+\'ll\s|[a-zA-Z]+\'re\s|[a-zA-Z]+\'ve\s|[a-zA-Z]+\'s\s|[a-zA-Z]+\'t\s'
+    lst_contractions = []
+    for i in range(len(dataset)):
+        post = dataset['post'][i]
+        find_contraction = re.findall(contraction, post)
+        total_contractions_sentence = len(find_contraction)
+        lst_contractions.append(total_contractions_sentence// len(dataset["post"][i].split()))
+    return lst_contractions
 
-combined_gen['capital count'], combined_gen['capital count (stand.)'] =  capital(combined_gen)
-combined_gen['emoticon count'], combined_gen['emoticon count (stand.)'] = emoticons(combined_gen)
-combined_gen['pronoun count'], combined_gen['pronoun count (stand.)'] = pronouns(combined_gen)
+
+def exaggerate(dataset):
+    exaggeration = '[a|A]{3,}|[b|B]{3,}|[c|C]{3,}|[d|D]{3,}|[e|E]{3,}|[f|F]{3,}|[g|G]{3,}|[h|H]{3,}|[i|I]{3,}|[j|J]{3,}|[k|K]{3,}|[l|L]{3,}|[m|M]{3,}[n|N]{3,}|[o|O]{3,}|[p|P]{3,}|[q|Q]{3,}|[r|R]{3,}|[s|S]{3,}|[t|T]{3,}|[u|U]{3,}|[v|V]{3,}|[w|W]{3,}|[x|X]{3,}|[y|Y]{3,}|[z|Z]{3,}|[.]{2,}|[!]{2,}'
+    lst_exaggeration = []
+    for i in range(len(dataset)):
+        post = dataset['post'][i]
+        find_exaggeration = re.findall(exaggeration, post)
+        total_exaggeration_sentence = len(find_exaggeration)
+        lst_exaggeration.append(total_exaggeration_sentence// len(dataset["post"][i].split()))
+    return lst_exaggeration
+
+
+def punctuation(dataset):
+    post_list = list(dataset["post"])
+    
+    punctuation_count = 0
+    punctuation_count_standardized_list = []
+
+    comma_count = 0
+    comma_count_standardized_list = []
+
+    exclamation_count = 0
+    exclamation_count_standardized_list = []
+
+    for post in tqdm(post_list):
+        for character in post:
+            if character in string.punctuation:
+                punctuation_count += 1
+            if character == ",":
+                comma_count += 1
+            if character == "!":
+                exclamation_count += 1
+        
+        punctuation_count_standardized_list.append(punctuation_count / len(post))
+        punctuation_count = 0
+
+        comma_count_standardized_list.append(comma_count / len(post))
+        comma_count = 0
+
+        exclamation_count_standardized_list.append(exclamation_count /len(post))
+        exclamation_count = 0
+    
+    return punctuation_count_standardized_list, comma_count_standardized_list, exclamation_count_standardized_list
+
+combined_gen['contraction count'] = contraction(combined_gen)
+combined_gen['exaggeration count'] = exaggerate(combined_gen)
+combined_gen['capital count'] =  capital(combined_gen)
+combined_gen['emoticon count'] = emoticons(combined_gen)
+combined_gen['pronoun count'] = pronouns(combined_gen)
+combined_gen['punctuation count'], combined_gen['comma count'], combined_gen['exclamation count'] = punctuation(combined_gen)
