7918154301136079e20438006b991400cbf412af
DaniChambon1 <d.chambon@student.tue.nl>
2023-12-20 17:06:41 +0100

    added eda plots

--- EDA.py
+++ EDA.py
@@ -6,16 +6,13 @@ from data_cleaning import combined_gen
 
 
 ### Get the number of posts made by males and females 
-grouped_data = combined_gen.groupby('female').size()
-
-count_male = grouped_data[0] if 0 in grouped_data.index else 0
-count_female = grouped_data[1] if 1 in grouped_data.index else 0
-
-print(f"The total number of posts is {len(combined_gen)}.The number of posts made by males is {count_male} and the number of posts made by females is {count_female}.")
+count_female = len(combined_gen[combined_gen["female"] == 0])
+count_male = len(combined_gen[combined_gen["female"] == 0])
+print(f"The total number of posts is {len(combined_gen)}. The number of posts made by males is {count_male} and the number of posts made by females is {count_female}.")
 
 
 ### Get the number of authors that are male and female
-grouped2 = combined_gen.groupby('auhtor_ID').agg({'female': ['min', 'max']})
+grouped2 = combined_gen.groupby('auhtor_ID').agg({'female': ['min', 'max'], 'Millennial': ['min']})
 
 # Check if there are no persons who are classified as both female and male
 no_double_classifications = grouped2['female', 'min'].equals(grouped2['female', 'max'])
@@ -38,9 +35,47 @@ print(f"The number of millenials is {nr_millennials}. The number of persons clas
 ### Get the number of millenials and gen z posts that are male vs female
 nr_female_millennials = len(combined_gen[(combined_gen["female"] == 1) & (combined_gen["Millennial"] == 1)])
 nr_female_genz = len(combined_gen[(combined_gen["female"] == 1) & (combined_gen["Millennial"] == 0)])
-print(f"The number of female millenials is {nr_female_millennials}. The number of females classified as gen z is {nr_female_genz}.")
+print(f"The number of female millenials posts is {nr_female_millennials}. The number of female posts classified as gen z is {nr_female_genz}.")
 
 nr_male_millennials = len(combined_gen[(combined_gen["female"] == 0) & (combined_gen["Millennial"] == 1)])
 nr_male_genz = len(combined_gen[(combined_gen["female"] == 0) & (combined_gen["Millennial"] == 0)])
-print(f"The number of male millenials is {nr_male_millennials}. The number of males classified as gen z is {nr_male_genz}")
+print(f"The number of male millenials posts is {nr_male_millennials}. The number of male posts classified as gen z is {nr_male_genz}")
 
+
+### Get the number of authors that are millenial and gen z for males and females
+
+nr_female_authors_millennial = len(grouped2[(grouped2['female']['min'] == 1) & (grouped2['Millennial']['min'] == 1)])
+nr_female_authors_genz = len(grouped2[(grouped2['female']['min'] == 1) & (grouped2['Millennial']['min'] == 0)])
+print(f"The number of authors that are female and millennial is {nr_female_authors_millennial}. The number of authors that are female and gen z is {nr_female_authors_genz}.")
+
+nr_male_authors_millennial = len(grouped2[(grouped2['female']['min'] == 0) & (grouped2['Millennial']['min'] == 1)])
+nr_male_authors_genz = len(grouped2[(grouped2['female']['min'] == 0) & (grouped2['Millennial']['min'] == 0)])
+print(f"The number of authors that are male and millennial is {nr_male_authors_millennial}. The number of authors that are male and gen z is {nr_male_authors_genz}.")
+
+
+
+### Make a graph of the distribution of ages in the dataset.
+
+#combined_gen["birth_year"].plot(kind='bar')
+# grouped_by = combined_gen.groupby('birth_year').size()
+# grouped_by.loc[2009] = 0
+# grouped_by = grouped_by.sort_index()
+# grouped_by.plot(kind='bar')
+# plt.xlabel('Birth Year')
+# plt.ylabel('Count')
+# plt.title('Count of Birth Years of posts', size = 16)
+# plt.show()
+
+
+grouped_by_gender = combined_gen.groupby(['birth_year', 'female']).size().unstack(fill_value=0)
+# Adding missing year (2009) with count 0 for both genders
+grouped_by_gender.loc[2009] = [0, 0]
+grouped_by_gender = grouped_by_gender.sort_index()
+ax = grouped_by_gender.plot(kind='bar', stacked=True)
+plt.xlabel('Birth Year')
+plt.ylabel('Count')
+plt.title('Count of Birth Years by Gender', size=16)
+plt.legend(['Male', 'Female'], loc='upper right')
+# Line is not shown??
+ax.axvline(x=1996, color='red', linestyle='--')
+plt.show()
