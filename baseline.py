2a0d8f2ffbfe43a3d8107c81f4737472f4b13ad2
DaniChambon1 <d.chambon@student.tue.nl>
2023-12-30 11:49:00 +0100

    added majority baseline without gender

--- baseline.py
+++ baseline.py
@@ -14,3 +14,13 @@ precision = precision_score(combined_gen['Millennial'], majority_predict)
 recall = recall_score(combined_gen['Millennial'], majority_predict)
 f1 = f1_score(combined_gen['Millennial'], majority_predict)
 print(f"Majority --> Accuracy: {accuracy}, Precision: {precision}, Recall: {recall}, F1-score: {f1}")
+
+# Now, majority prediction will be done without gender as a predictor
+
+majority_predict2 = [1] * len(combined_gen)
+
+accuracy2 = accuracy_score(combined_gen['Millennial'], majority_predict2)
+precision2 = precision_score(combined_gen['Millennial'], majority_predict2)
+recall2 = recall_score(combined_gen['Millennial'], majority_predict2)
+f12 = f1_score(combined_gen['Millennial'], majority_predict2)
+print(f"Majority predict without gender --> Accuracy: {accuracy2}, Precision: {precision2}, Recall: {recall2}, F1-score: {f12}")
